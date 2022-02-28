bf76049bfe0005c421cd1a91671b6b5d84b40d68
mauro <mauro.casadei@soluzionesoftware.com>
2022-02-28 15:03:31 +0100

    migiorata documentazione

--- lezioni/lezione6/login.php
+++ lezioni/lezione6/login.php
@@ -1,15 +1,20 @@
 <!-- 
     creare un form con 2 input: login e password + submit
     ricevere i dati del form
-    if se la password è uguale a mia-pwd (che è password corretta) 
-        setcookie istanziare un cookie e scrivere "benvenuto utente" [cookie con durata 1 minuto]
-    else
-        if se esiste il cookie
+    verificare se ricevo parametro password, successivamente:
+
+        if se la password è uguale a mia-pwd (che è password corretta) 
+            setcookie istanziare un cookie e scrivere "benvenuto utente" [cookie con durata 1 minuto]
+        else
+        
+        end if
+    se non ho ricevuto il parametro password
+     if se esiste il cookie
             print  "utente già loggato"
         else
             print "utente non loggato
-        end if
-    end if
+     end if
+    ----
  -->
  <?php 
         $messaggio = null;
