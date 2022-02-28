05609c3585ca8c3ee28293fddeb3264e5504cc1c
mauro <mauro.casadei@soluzionesoftware.com>
2022-02-28 17:44:16 +0100

    mysql parte 1

--- lezioni/lezione6/lezione6.php
+++ lezioni/lezione6/lezione6.php
@@ -1,22 +1,59 @@
 <?php 
 //lettura da database
-// connessione
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test";
-//stile procedurale
-$conn = mysqli_connect($host, $username, $password, $dbname);
-print_r($conn);
-if (! mysqli_connect_errno( $conn ) ) {
-    print "Connessione avvenuta con successo";
-}else{
-    echo "ERRORE in Connessione a MySQL: " . mysqli_connect_error();
+$conn = null;
+if (! connetti()){
+    die ("ERRORE");
+}
+if (! leggi()){
+    die ("ERRORE");
+}
+if (! disconnetti()){
+    die ("ERRORE");
 }
 
-//query
 
-//fetch
 
-//close
-mysqli_close($conn);
+
+
+function connetti(){
+    global $conn;
+    // connessione
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test";
+    //stile procedurale
+    $conn = mysqli_connect($host, $username, $password, $dbname);
+    //print_r($conn);
+    if (! mysqli_connect_errno() ) {
+        print "Connessione avvenuta con successo";
+    }else{
+        echo "ERRORE in Connessione a MySQL: " . mysqli_connect_error();
+        return false;
+    }
+    return true;
+}
+function leggi(){
+    global $conn;
+    //query
+    print "\n";
+    $result = mysqli_query($conn, "SELECT * FROM utenti WHERE id_utente < 10");
+    if (! $result){
+        print ("Errore nella Query:" . mysqli_error($conn));
+        return false;
+    }
+    //fetch
+    print "\n";
+    while ($row = mysqli_fetch_assoc($result)){
+        printf ("Cognome:%s - Nome:%s \n", $row["nome"], $row["cognome"]);
+    }
+    return true;
+}
+function disconnetti(){
+    global $conn;
+    //close
+    if (! mysqli_close($conn)){
+        return false;
+    }
+    return true;
+}
