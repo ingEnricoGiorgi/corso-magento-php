567cfa20191b6fd31bf1bee1a3dfbe043a2307c9
mauro <mauro.casadei@soluzionesoftware.com>
2022-02-28 14:43:48 +0100

    esercizi

---
+++ lezioni/lezione6/login.php
@@ -0,0 +1,13 @@
+<!-- 
+    creare un form con 2 input: login e password + submit
+    ricevere i dati del form
+    if se la password è uguale a mia-pwd (che è password corretta) 
+        setcookie istanziare un cookie e scrivere "benvenuto utente" [cookie con durata 1 minuto]
+    else
+        if se esiste il cookie
+            print  "utente già loggato"
+        else
+            print "utente non loggato
+        end if
+    end if
+ -->
